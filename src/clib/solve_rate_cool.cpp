fdf2e76ae8c7bdd6eb9e117c3b06bb0511565427
Matthew Abruzzo <matthewabruzzo@gmail.com>
2025-12-08 20:48:59 -0500

    tweak signature of make_consistent

--- src/clib/solve_rate_cool.cpp
+++ src/clib/solve_rate_cool.cpp
@@ -1035,7 +1035,9 @@ int solve_rate_cool(
 
     // Correct the species to ensure consistency (i.e. type conservation)
 
-    grackle::impl::make_consistent(imetal, dom, my_chemistry, my_rates, my_fields);
+    grackle::impl::make_consistent(
+        imetal, dom, my_chemistry,
+        my_rates->opaque_storage->inject_pathway_props, my_fields);
 
   }
 
